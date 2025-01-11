bea7b3535a23aeee51b87deb567d77b8abd73d23
Joshua Nussbaum <67082011+joshuanussbaum@users.noreply.github.com>
2025-01-11 17:52:39 -0600

    fix tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,12 +1,13 @@
 package auth
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 	"testing"
 )
 
-func testGetAPIKey(t *testing.T) {
+func TestGetAPIKey(t *testing.T) {
 	req, _ := http.NewRequest("GET", "http://www.example.com", strings.NewReader("hello"))
 	empty, err := GetAPIKey(req.Header)
 	if empty != "" || err != ErrNoAuthHeaderIncluded {
@@ -22,9 +23,10 @@ func testGetAPIKey(t *testing.T) {
 	if long != "" || err == nil {
 		t.Fatalf("expected failure, got str %s and err %v", long, err)
 	}
-	req.Header.Set("Authorization", "Bearer foobar1234567890")
+	apiToken := "foobar1234567890"
+	req.Header.Set("Authorization", fmt.Sprintf("ApiKey %s", apiToken))
 	token, err := GetAPIKey(req.Header)
 	if token != "foobar1234567890" || err != nil {
-		t.Fatalf("expected token 'foobar1234567890', got str %s and err %v", long, err)
+		t.Fatalf("expected token 'foobar1234567890', got str %s and err %v", token, err)
 	}
 }
